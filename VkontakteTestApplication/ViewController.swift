d84a6028181e5cdd1dc43a04f03a5f8896f7162d
Nikita Lezya <nikitalezya@gmail.com>
2018-05-26 12:22:47 +0300

    Initial Commit

---
+++ VkontakteTestApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VkontakteTestApplication
+//
+//  Created by John Dowland on 26.05.2018.
+//  Copyright © 2018 Nikita Lezya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
